b4b8d7457fe53568ece44813c9add99a0ca0d46c
Felipe Peter <mr-peipei@web.de>
2020-05-24 15:06:57 +0800

    Implement animation but not starting from the right end

--- lib/characterAnimator.dart
+++ lib/characterAnimator.dart
@@ -1,16 +1,47 @@
 import 'dart:convert';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:svg_path_parser/svg_path_parser.dart';
 
-class CharacterAnimator extends StatelessWidget {
+class CharacterAnimator extends StatefulWidget {
   final String strokeOrder;
 
   CharacterAnimator(this.strokeOrder);
 
   @override
+  _CharacterAnimatorState createState() => _CharacterAnimatorState();
+}
+
+class _CharacterAnimatorState extends State<CharacterAnimator>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    _controller.stop();
+    _controller.reset();
+    _controller.repeat(
+      period: Duration(seconds: 5),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var parsedJson = json.decode(strokeOrder.replaceAll("'", '"'));
+    var parsedJson = json.decode(widget.strokeOrder.replaceAll("'", '"'));
 
     final List<Path> strokes = List.generate(
         parsedJson['strokes'].length,
@@ -20,34 +51,75 @@ class CharacterAnimator extends StatelessWidget {
 
     return Column(
       children: <Widget>[
-        Text('Number of strokes: ' +
-            parsedJson['strokes'].length.toString() +
-            '\nFirst stroke: ' +
-            parsedJson['strokes'][0]),
-        showCharacter(strokes, Colors.grey),
+        Text('Number of strokes: ' + strokes.length.toString()),
+        MaterialButton(
+          onPressed: () {
+            _startAnimation();
+          },
+          child: Text("Animate"),
+        ),
+        FittedBox(
+          child: SizedBox(
+            width: 1024,
+            height: 1024,
+            child: CustomPaint(
+                painter: StrokePainter(strokes[0], strokeColor: Colors.blue, outlineColor: Colors.red,
+                    animate: true, animation: _controller, showOutline: true)),
+          ),
+        ),
       ],
     );
   }
 }
 
-class MyPainter extends CustomPainter {
-  final Path path;
-  final Color color;
-  final bool showPath;
-  MyPainter(this.path, this.color, {this.showPath = true});
+class StrokePainter extends CustomPainter {
+  final bool animate;
+  final Animation<double> animation;
+  final Path strokeOutlinePath;
+  final Color strokeColor;
+  final Color outlineColor;
+  final bool showOutline;
+
+  StrokePainter(
+    this.strokeOutlinePath, {
+    this.strokeColor = Colors.grey,
+    this.showOutline = false,
+    this.outlineColor = Colors.black,
+    this.animate = false,
+    this.animation,
+  }) : super(repaint: animation);
 
   @override
   void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = color
-      ..strokeWidth = 4.0;
-    canvas.drawPath(path, paint);
-    if (showPath) {
-      var border = Paint()
-        ..color = Colors.black
+    var strokePaint = Paint()
+      ..color = strokeColor
+      ..strokeWidth = 4.0
+      ..style = PaintingStyle.fill;
+
+    if (animate == true && animation != null) {
+      PathMetric pathMetric = strokeOutlinePath.computeMetrics().toList()[0];
+      double pathLength = pathMetric.length;
+      double pathLengthFraction = animation.value / 2 * pathLength;
+
+      Path startPath = pathMetric.extractPath(0, pathLengthFraction);
+      Path endPath =
+          pathMetric.extractPath(pathLength - pathLengthFraction, pathLength);
+
+      Path drawPath = Path.combine(PathOperation.union, startPath, endPath);
+      canvas.drawPath(drawPath, strokePaint);
+    }
+    else {
+      canvas.drawPath(strokeOutlinePath, strokePaint);
+    }
+
+
+
+    if (showOutline) {
+      var borderPaint = Paint()
+        ..color = outlineColor
         ..strokeWidth = 1.0
         ..style = PaintingStyle.stroke;
-      canvas.drawPath(path, border);
+      canvas.drawPath(strokeOutlinePath, borderPaint);
     }
   }
 
@@ -55,21 +127,20 @@ class MyPainter extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
-showCharacter(List<Path> strokes, Color color) {
-  return Expanded(
-      child: Stack(
-    children: <Widget>[
-      ...List.generate(
-        strokes.length,
-        (index) => FittedBox(
-          child: SizedBox(
-            width: 1024,
-            height: 1024,
-            child: CustomPaint(
-                painter: MyPainter(strokes[index], color, showPath: false)),
-          ),
-        ),
-      ),
-    ],
-  ));
-}
+// showCharacter(List<Path> strokes, Color color) {
+//   return Expanded(
+//       child: Stack(
+//     children: <Widget>[
+//       ...List.generate(
+//         strokes.length,
+//         (index) => FittedBox(
+//           child: SizedBox(
+//             width: 1024,
+//             height: 1024,
+//             child: CustomPaint(painter: StrokePainter(strokes[index], color)),
+//           ),
+//         ),
+//       ),
+//     ],
+//   ));
+// }
