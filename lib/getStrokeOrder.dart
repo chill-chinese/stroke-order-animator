217d19f5c9238679921516eb9083cb0fa348c1da
Felipe Peter <mr-peipei@web.de>
2024-01-09 15:21:28 +0100

    feat: Add `getStrokeOrder` (#35)

---
+++ lib/getStrokeOrder.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart';
+
+Future<String> getStrokeOrder(String character, Client client) async {
+  const baseUrl = "https://cdn.jsdelivr.net/npm/hanzi-writer-data@2.0.1/";
+
+  final url = Uri.parse(baseUrl + character + ".json");
+
+  final response = await client.get(url);
+
+  if (response.statusCode != 200) {
+    throw Exception("Failed to get stroke order for $character");
+  }
+
+  return response.body;
+}
---
+++ test/getStrokeOrder_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:stroke_order_animator/getStrokeOrder.dart';
+
+import 'getStrokeOrder_test.mocks.dart';
+import 'testStrokes.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  test('Test existing stroke order retrieval', () async {
+    final client = MockClient();
+
+    when(client.get(any)).thenAnswer((_) async {
+      return http.Response(strokeOrders[0], 200);
+    });
+    await getStrokeOrder("永", client);
+
+    verify(client
+        .get(Uri.parse("https://cdn.jsdelivr.net/npm/hanzi-writer-data@2.0.1/"
+            "永.json")));
+  });
+
+  test('Test non-existing stroke order retrieval', () async {
+    final client = MockClient();
+    when(client.get(any)).thenAnswer((_) async => http.Response("", 404));
+    expect(
+        () async => await getStrokeOrder("a", client),
+        throwsA(predicate((e) =>
+            e is Exception &&
+            e.toString() == "Exception: Failed to get stroke order for a")));
+  });
+}
